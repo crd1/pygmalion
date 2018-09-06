30be7f8cf1b50022dac363ef42b71cd0f7342ea6
crd <christopher@rudoll.net>
2018-09-06 16:18:54 +0200

    added reset command

---
+++ src/main/kotlin/net/rudoll/pygmalion/handlers/reset/ResetHandler.kt
@@ -0,0 +1,32 @@
+package net.rudoll.pygmalion.handlers.reset
+
+import net.rudoll.pygmalion.handlers.Handler
+import net.rudoll.pygmalion.handlers.arguments.parsedarguments.ParsedArgument
+import net.rudoll.pygmalion.model.Action
+import net.rudoll.pygmalion.model.Input
+import net.rudoll.pygmalion.model.ParseStage
+import net.rudoll.pygmalion.model.ParsedInput
+import spark.Spark
+
+object ResetHandler : Handler {
+    override fun handle(input: Input, parsedInput: ParsedInput) {
+        parsedInput.actions.add(object : Action {
+            override fun run(arguments: Set<ParsedArgument>) {
+                Spark.stop()
+            }
+        })
+        input.consume(1)
+    }
+
+    override fun canHandle(input: Input): Boolean {
+        return input.first() == "reset"
+    }
+
+    override fun getDocumentation(): String {
+        return "reset"
+    }
+
+    override fun getParseStage(): ParseStage {
+        return ParseStage.FIRST_PASS
+    }
+}
