d72f5cde39a4e2518a5f7387968481c1546f591c
crd <christopher@rudoll.net>
2019-10-10 16:11:03 +0200

    first well-formed openapi spec

--- src/main/kotlin/net/rudoll/pygmalion/model/StateHolder.kt
+++ src/main/kotlin/net/rudoll/pygmalion/model/StateHolder.kt
@@ -1,9 +1,10 @@
 package net.rudoll.pygmalion.model
 
 import io.swagger.v3.oas.models.OpenAPI
+import net.rudoll.pygmalion.handlers.openapi.export.OpenApiMonitor
 
 object StateHolder {
-    data class State(var portSet: Boolean = false, var chaosMonkeyProbability: Int = 0, var openAPISpec: OpenAPI = OpenAPI())
+    data class State(var portSet: Boolean = false, var chaosMonkeyProbability: Int = 0, var openAPISpec: OpenAPI = OpenApiMonitor.getPrototype())
 
     var state = State()
 
