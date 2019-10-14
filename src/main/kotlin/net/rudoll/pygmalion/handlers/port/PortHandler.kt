94849a305f618e5b12a75612fac1216487da6100
crd <christopher@rudoll.net>
2019-10-14 13:03:38 +0200

    improved PortHandler implementation

--- src/main/kotlin/net/rudoll/pygmalion/handlers/port/PortHandler.kt
+++ src/main/kotlin/net/rudoll/pygmalion/handlers/port/PortHandler.kt
@@ -1,10 +1,8 @@
 package net.rudoll.pygmalion.handlers.port
 
 import net.rudoll.pygmalion.handlers.Handler
-import net.rudoll.pygmalion.model.Input
-import net.rudoll.pygmalion.model.ParseStage
-import net.rudoll.pygmalion.model.ParsedInput
-import net.rudoll.pygmalion.model.StateHolder
+import net.rudoll.pygmalion.handlers.arguments.parsedarguments.ParsedArgument
+import net.rudoll.pygmalion.model.*
 import net.rudoll.pygmalion.util.PortUtil
 import spark.Spark
 
@@ -12,6 +10,7 @@ object PortHandler : Handler {
     override fun getParseStage(): ParseStage {
         return ParseStage.FIRST_PASS
     }
+
     override fun getDocumentation(): String {
         return "port \$port"
     }
@@ -21,11 +20,25 @@ object PortHandler : Handler {
     }
 
     override fun handle(input: Input, parsedInput: ParsedInput) {
-        if (StateHolder.state.portSet) {
-            parsedInput.errors.add("Port was already set. Please use another instance of this application.")
+        input.consume(1)
+        if (!input.hasNext()) {
+            parsedInput.errors.add("No port specified")
             return
         }
-        PortUtil.setPort(input.second().toInt())
-        input.consume(2)
+        val port = input.first().toIntOrNull()
+        input.consume(1)
+        if (port == null) {
+            parsedInput.errors.add("Port is not numeric")
+            return
+        }
+        parsedInput.actions.add(object : Action {
+            override fun run(arguments: Set<ParsedArgument>) {
+                if (StateHolder.state.portSet) {
+                    parsedInput.errors.add("Port was already set. Please use another instance of this application.")
+                    return
+                }
+                PortUtil.setPort(port)
+            }
+        })
     }
 }
