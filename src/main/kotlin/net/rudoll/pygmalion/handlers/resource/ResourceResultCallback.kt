f4649ba256434713325a175b79f58cdbbd725763
crd <christopher@rudoll.net>
2019-12-12 09:38:40 +0100

    reduced field visibility

--- src/main/kotlin/net/rudoll/pygmalion/handlers/resource/ResourceResultCallback.kt
+++ src/main/kotlin/net/rudoll/pygmalion/handlers/resource/ResourceResultCallback.kt
@@ -2,7 +2,7 @@ package net.rudoll.pygmalion.handlers.resource
 
 import net.rudoll.pygmalion.common.HttpCallMapper
 
-abstract class ResourceResultCallback(protected val baseResultCallbackDescription: HttpCallMapper.ResultCallback.ResultCallbackDescription) : HttpCallMapper.ResultCallback {
+abstract class ResourceResultCallback(private val baseResultCallbackDescription: HttpCallMapper.ResultCallback.ResultCallbackDescription) : HttpCallMapper.ResultCallback {
     override fun getResultCallbackDescription(): HttpCallMapper.ResultCallback.ResultCallbackDescription? {
         return this.baseResultCallbackDescription
     }
